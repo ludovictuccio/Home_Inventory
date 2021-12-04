8e54ea78c8349ad08b42da2babf4ad341ce5bf1e
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2021-12-04 22:33:46 +0100

    Initial code

---
+++ src/test/java/com/home/inventory/HomeInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
