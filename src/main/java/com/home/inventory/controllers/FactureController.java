81ab9ff9bafeb5b2a39968e4017296282fe26e38
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2022-01-02 22:02:05 +0100

    Facture controller
    - With IT and templates
    - A problem with all update processes for controllers requests

---
+++ src/main/java/com/home/inventory/controllers/FactureController.java
@@ -0,0 +1,93 @@
+package com.home.inventory.controllers;
+
+import javax.validation.Valid;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.home.inventory.entities.Facture;
+import com.home.inventory.repository.FactureRepository;
+import com.home.inventory.services.IFactureService;
+
+@Controller
+@RequestMapping("/factures")
+public class FactureController {
+
+    private static final Logger LOGGER = LogManager
+            .getLogger("FactureController");
+
+    @Autowired
+    private IFactureService factureService;
+
+    @Autowired
+    private FactureRepository factureRepository;
+
+    @GetMapping("/list")
+    public String home(final Model model) {
+        model.addAttribute("factures", factureRepository.findAll());
+        return "factures/list";
+    }
+
+    @GetMapping("/add")
+    public String addFacture(final Model model) {
+        model.addAttribute("facture", new Facture());
+        return "factures/add";
+    }
+
+    @PostMapping("/validate")
+    public String validate(@Valid @ModelAttribute("facture") Facture facture,
+            final BindingResult result, final Model model) {
+        if (!result.hasErrors()) {
+            Facture factureToAdd = factureService.addFacture(facture);
+            model.addAttribute("facture", factureToAdd);
+            return "redirect:/factures/list";
+        }
+        LOGGER.info("POST request FAILED for: /factures/validate");
+        return "factures/add";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") final Long id,
+            final Model model) {
+        Facture facture = factureService.getFactureById(id);
+
+        if (facture == null) {
+            LOGGER.error("Invalid facture Id: {}", id);
+            return "redirect:/factures/list";
+        }
+        model.addAttribute("facture", facture);
+        return "factures/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateFacture(@PathVariable("id") final Long id,
+            @Valid @ModelAttribute("category") Facture facture,
+            final BindingResult result, final Model model) {
+        if (result.hasErrors()) {
+            LOGGER.info("POST request FAILED for: /factures/update/{id}");
+            return "factures/update";
+        }
+        // facture.setId(id);
+        factureService.updateFactureById(facture, id);
+        model.addAttribute("facture", facture);
+        return "redirect:/factures/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteFacture(@PathVariable("id") final Long id,
+            final Model model) {
+        factureService.deleteFactureById(id);
+        model.addAttribute("factures", factureRepository.findAll());
+        return "redirect:/factures/list";
+    }
+
+}
